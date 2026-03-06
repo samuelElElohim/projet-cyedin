44e7a043e69f2fb9d53503c1551cd7fffdf20eb1
Samuel Emile Jérôme Boucheron Seguin <samuelbs364@gmail.com>
2026-03-06 15:31:17 +0100

    test: changement de la page '/'

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+/*
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -12,7 +13,13 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
+});*/
+
+
+Route::get('/', function () {
+    return Inertia::render('Home');
 });
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
