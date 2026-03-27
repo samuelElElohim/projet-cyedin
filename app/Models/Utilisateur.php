15d0b4c33b183d90184589e40618fc824e15ba5e
Samuel Emile Jérôme Boucheron Seguin <samuelbs364@gmail.com>
2026-03-27 14:51:05 +0100

    fetch des models de younes

---
+++ app/Models/Utilisateur.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Utilisateur extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'email',
+        'mot_de_passe',
+        'role', // role doit avoir un seul char
+        // imposer que si le role =/= d'entreprise, alors le nom et prenom doivent etre =/= null
+        'est_active'
+    ];
+
+    protected $guarded =
+    [
+        'id', // doit etre imposer numeriquement par le system lorsqu'un utilisateur est cree
+
+        'date_creation' // la date de creation d'un compte ne doit pas etre modifiable
+    ];
+
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'mot_de_passe',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'mot_de_passe' => 'hashed',
+        ];
+    }
+}
